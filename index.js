55a2d41a4f7fbd7746ae75914f3a68523cea75ed
miamartinezfe <miguelshaddix95@hotmail.com>
2023-05-30 10:54:55 -0500

    function personalData

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function laCajaDePandora(numero){
+function miguel(numero){
 // proximamente escribiremos codigo aqui
 // sdfdsfdsjf
+return {
+    name: 'Miguel Martinez',
+    age: 27,
+    origin: 'Colombia'
+}
 }
