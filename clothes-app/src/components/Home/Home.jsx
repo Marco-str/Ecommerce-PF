d92e0f33e21fce5ba7352639de30b5d29ebc694d
Lucia Lisdero <lulisdero20@gmail.com>
2023-05-31 17:07:27 -0300

    paginated + home

--- clothes-app/src/components/Home/Home.jsx
+++ clothes-app/src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
 import CardsComponent from '../CardsComponent/CardsComponent';
 import Nav from '../Nav/Nav';
-import Paginated from '../Paginated/Paginated'
+import Paginated from '../Paginated/Paginated';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Home(){
+ const [productPerPage] = useState(12);
 
+ const allProducts = useSelector((state) => state.products);
     return(
     <div>
 <Nav/>
-<Paginated/>
+<Paginated   productPerPage={productPerPage}
+            allProducts={allProducts.length}
+            paginated={paginated}/>
 <CardsComponent/>
     </div>
 
