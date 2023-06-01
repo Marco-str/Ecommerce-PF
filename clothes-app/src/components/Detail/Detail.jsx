d073c7da0b1d4c9ffebdee0765893dde633bb4c5
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-06-01 16:28:00 -0600

    details primeros pasos

---
+++ clothes-app/src/components/Detail/Detail.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+import styles from './detail.module.css';
+
+export default function Detail(){
+    return(<>
+        <div className={styles.galeria}>
+            {/* mapeamos las img de ropa */}
+        </div>
+        <div className={styles.container}>
+            {/* contenedor de elementos */}
+            <div>
+                {/* Aqui va un buy now con el precio */}
+            </div>
+            <div>
+                {/* Caracteristicas */}
+            </div>
+            <div>
+                {/* boton de añadir a cart */}
+            </div>
+            <div>
+                {/* boton de regreso */}
+            </div>
+        </div>
+    </>);
+}
