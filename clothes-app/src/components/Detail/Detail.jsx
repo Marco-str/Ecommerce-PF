571e40bd45dda4b7de3b93e938add72da4d447ab
OscarRangelM <orangelm1300@alumno.ipn.mx>
2023-06-02 07:43:23 -0600

    funcionMau

--- clothes-app/src/components/Detail/Detail.jsx
+++ clothes-app/src/components/Detail/Detail.jsx
@@ -1,27 +1,50 @@
 import React from "react";
 
+import { FaCartArrowDown, FaArrowLeft } from "react-icons/fa";
 
 import styles from './detail.module.css';
 
-export default function Detail(){
-    return(<>
-        <div className={styles.galeria}>
-            {/* mapeamos las img de ropa */}
+export default function Detail() {
+    return (
+        <div className={styles.mainContainer}>
+        
+        <div className={styles.productImg}>
+          <h3>Nombre del producto</h3>
+          <div className={styles.img}></div>
         </div>
-        <div className={styles.container}>
-            {/* contenedor de elementos */}
+
+        <div className={styles.details}>
+          <div className={styles.buyNow}>
+            <h3>BUY NOW</h3>
+            <h3>$5000</h3>
+          </div>
+          <div className={styles.colorSize}>
             <div>
-                {/* Aqui va un buy now con el precio */}
+              <label htmlFor="color">
+                Color:
+              </label>
+              <button className={styles.color} style={{backgroundColor: 'white'}}></button>
+              <button className={styles.color} style={{backgroundColor: 'red'}}></button>
+              <button className={styles.color} style={{backgroundColor: 'blue'}}></button>
             </div>
             <div>
-                {/* Caracteristicas */}
+              <label htmlFor="colors">Color: </label>
+              <input type="color" name={styles.colors} id="" />
             </div>
             <div>
-                {/* boton de añadir a cart */}
+              <label htmlFor="color">
+                Size:
+              </label>
+              <button className={styles.size}>S</button>
+              <button className={styles.size}>M</button>
+              <button className={styles.size}>L</button>
             </div>
-            <div>
-                {/* boton de regreso */}
-            </div>
+          </div >
+        <div className={styles.cart}>
+            <button>Add to Cart <FaCartArrowDown className={styles.icon}></FaCartArrowDown></button>
+            <button>Back <FaArrowLeft className={styles.icon}></FaArrowLeft></button>
         </div>
-    </>);
+        </div >
+      </div >
+    );
 }
