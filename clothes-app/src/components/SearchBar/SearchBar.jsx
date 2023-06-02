55061c8c30f1f9525a2b65951f7f727cd8ab8f7c
Lucia Lisdero <lulisdero20@gmail.com>
2023-06-02 18:00:45 -0300

    search

--- clothes-app/src/components/SearchBar/SearchBar.jsx
+++ clothes-app/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { getNamePokemon } from "../../redux/action/index";
+
+import { getProductByName } from "../../redux/actions/actions";
+
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
@@ -17,7 +19,11 @@ export default function SearchBar() {
     if (name.trim()) {
       // verifica si el valor del campo de entrada no está vacío
       console.log(name);
-      dispatch(getNamePokemon(name));
+<<<<<<< HEAD
+      dispatch(getAllProducts(name));
+=======
+      dispatch(getProductByName(name));
+>>>>>>> developer
       setName(""); // limpia el valor del campo de entrada después de enviar la acción
     } else {
       alert("Enter a valid name");
