895af13dd0b7e14e61fee83a50a76128b3f2df48
Lucass2160 <lucas-cali@hotmail.com>
2023-06-03 13:48:51 -0300

    se agrego nav y cambios en la cards

--- clothes-app/src/components/Card/Card.jsx
+++ clothes-app/src/components/Card/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ name, image }) => {
   // Desestructurar la prop 'name' de las props
   return (
     <div className={style.mainContainer}>
-      <p>{name}</p>
+      <p className={style.title}>{name}</p>
       <img className={style.card} src={image} alt="" />
       {<Link>Learn more</Link>}
     </div>
