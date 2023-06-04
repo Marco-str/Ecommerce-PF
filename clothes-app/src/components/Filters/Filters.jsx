ff1bef4092213aac4c57b4d994881bef14a293b2
TomasZucchi <tomaszucchi0@gmail.com>
2023-06-04 01:27:56 -0300

    filter

--- clothes-app/src/components/Filters/Filters.jsx
+++ clothes-app/src/components/Filters/Filters.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { filterByPrice, filterByCategory } from '../../redux/actions/actions';
 import styles from './Filters.module.css';
@@ -8,32 +8,24 @@ const Filter = () => {
   const [priceRange, setPriceRange] = useState([0, 1000]);
   const products = useSelector(state => state.products);
   const [selectedCategory, setSelectedCategory] = useState('');
-  const [uniqueCategories, setUniqueCategories] = useState([]);
-
-  useEffect(() => {
-    const categories = Array.from(new Set(products.map(product => product.category)));
-    setUniqueCategories(categories);
-  }, [products]);
 
   const handlePriceChange = event => {
-    const maxPrice = Number(event.target.value);
-    setPriceRange([0, maxPrice]);
+    setPriceRange([0, event.target.value]);
   };
 
-  const handleApplyFilter = () => {
+  const handleFilterByPrice = () => {
     dispatch(filterByPrice(priceRange));
-    dispatch(filterByCategory(selectedCategory));
   };
 
   const handleResetFilter = () => {
     setPriceRange([0, 1000]);
-    setSelectedCategory('');
-    window.location.reload(); // Restablecer la página
+    // Dispatch an action to reset the filters in your reducer
   };
 
   const handleCategoryChange = event => {
     const selectedCategory = event.target.value;
     setSelectedCategory(selectedCategory);
+    dispatch(filterByCategory(selectedCategory));
   };
 
   return (
@@ -48,9 +40,15 @@ const Filter = () => {
           min={0}
           max={1000}
         />
-        <div className={styles['price-range-text']}>
-          <p>{`$0 - $${priceRange[1]}`}</p>
-        </div>
+        <button onClick={handleFilterByPrice}>Apply</button>
+      </div>
+      <div className={styles['filter-section']}>
+        <button
+          className={styles['reset-button']}
+          onClick={handleResetFilter}
+        >
+          Reset Filter
+        </button>
       </div>
       <div className={styles['filter-section']}>
         <h4>Categories</h4>
@@ -60,21 +58,12 @@ const Filter = () => {
           onChange={handleCategoryChange}
         >
           <option value="">--Select--</option>
-          {uniqueCategories.map(category => (
-            <option value={category} key={category}>
-              {category}
+          {products.map(product => (
+            <option value={product.category} key={product.category}>
+              {product.category}
             </option>
           ))}
         </select>
-      </div>
-      <div className={styles['filter-section']}>
-        <button onClick={handleApplyFilter}>Apply</button>
-        <button
-          className={styles['reset-button']}
-          onClick={handleResetFilter}
-        >
-          Reset Filter
-        </button>
       </div>
     </div>
   );
