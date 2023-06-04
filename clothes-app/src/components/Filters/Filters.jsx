879f63b215e4dd550dd3603d8eec7516ae7c5df7
TomasZucchi <tomaszucchi0@gmail.com>
2023-06-04 19:07:12 -0300

    Price.

--- clothes-app/src/components/Filters/Filters.jsx
+++ clothes-app/src/components/Filters/Filters.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { filterByCategory, resetFilters } from '../../redux/actions/actions';
+import { filterByCategory, filterByPrice, resetFilters } from '../../redux/actions/actions';
 import styles from './Filters.module.css';
 
 const Filters = () => {
@@ -8,6 +8,7 @@ const Filters = () => {
   const products = useSelector(state => state.products);
   const [selectedCategory, setSelectedCategory] = useState('');
   const [uniqueCategories, setUniqueCategories] = useState([]);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
 
   useEffect(() => {
     // Obtener las categorías únicas de los productos
@@ -18,6 +19,7 @@ const Filters = () => {
 
   const handleResetFilter = () => {
     setSelectedCategory('');
+    setPriceRange([0, 1000]);
     dispatch(resetFilters());
   };
 
@@ -25,6 +27,20 @@ const Filters = () => {
     const selectedCategory = event.target.value;
     setSelectedCategory(selectedCategory);
     dispatch(filterByCategory(selectedCategory));
+  };
+
+  const handleMinPriceChange = event => {
+    const minPrice = parseInt(event.target.value, 10);
+    setPriceRange([minPrice, priceRange[1]]);
+  };
+
+  const handleMaxPriceChange = event => {
+    const maxPrice = parseInt(event.target.value, 10);
+    setPriceRange([priceRange[0], maxPrice]);
+  };
+
+  const handleFilterByPrice = () => {
+    dispatch(filterByPrice(priceRange));
   };
 
   return (
@@ -46,6 +62,31 @@ const Filters = () => {
         </select>
       </div>
       <div className={styles['filter-section']}>
+        <h4>Price Range</h4>
+        <div className={styles['price-input']}>
+          <input
+            type="number"
+            value={priceRange[0]}
+            onChange={handleMinPriceChange}
+            min={0}
+            max={priceRange[1]}
+            key="minPrice"
+          />
+          <span>$</span>
+          <span> - </span>
+          <input
+            type="number"
+            value={priceRange[1]}
+            onChange={handleMaxPriceChange}
+            min={priceRange[0]}
+            max={1000}
+            key="maxPrice"
+          />
+          <span>$</span>
+          <button onClick={handleFilterByPrice}>Apply</button>
+        </div>
+      </div>
+      <div className={styles['filter-section']}>
         <button
           className={styles['reset-button']}
           onClick={handleResetFilter}
@@ -58,4 +99,3 @@ const Filters = () => {
 };
 
 export default Filters;
-
