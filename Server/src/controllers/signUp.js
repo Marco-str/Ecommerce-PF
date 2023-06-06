a945bc5a968e4793ea9fd5131d2b31a2c65d1a5c
Marco Salazar <122067295+Marco-str@users.noreply.github.com>
2023-06-06 16:51:19 -0300

    Back/refactor/JWTready

--- Server/src/controllers/signUp.js
+++ Server/src/controllers/signUp.js
@@ -1,34 +1,38 @@
 const { User } = require("../db");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 
 
 
 const signUp = async (req, res) => {
 try{
-    const {name, phone, email, password} = req.body;
+    const {name, phone, email, password, admin} = req.body;
 
     const emailExist = await User.findOne({where: {email: email}});
-    
+    if(!email || !password || !name || !phone){
+        return res.status(400).json({msg: "PLease, all fill are required"});
+    }
     if(emailExist){
         return res.status(400).json({msg: "Email already exist"});
 
 }
-    
-    const salt = await bcrypt.genSalt(69);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
+;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const newUser = await User.create({name, phone, email, password: hashedPassword});
+console.log(newUser);
+    const token = jwt.sign({id: newUser.id, name: newUser.name, email: newUser.email}, `${JWT_SECRET}`);
 
-    res.status(200).json({msg: newUser});
+    // res.status(200).json({msg: newUser});
+    res.status(200).json({token});
 
 
 
-}catch(error){
-    console.log(error);
+    } catch(error){
     res.status(500).json(error.message);
-}
+        }
 };
 
 
